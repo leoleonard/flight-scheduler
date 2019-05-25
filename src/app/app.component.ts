5b015a994871dd94e39710dc8475d83286f8bc30
leoleonard <damian.juzyk@gmail.com>
2019-05-25 15:07:55 +0200

    add display flights + registrations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { IFlight } from "src/app/models/flight.interface";
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {IFlight} from 'src/app/models/flight.interface';
+import {FlightsService} from './services/flights.service';
+import {RegistrationsService} from './services/registrations.service';
 
 @Component({
     encapsulation: ViewEncapsulation.None,
-    selector: "app-root",
-    templateUrl: "./app.component.html",
+    selector: 'app-root',
+    templateUrl: './app.component.html',
     styleUrls: [
-        "../assets/css/reset.css",
-        "../assets/css/fonts.css"
-  ]
+        '../assets/css/reset.css',
+        '../assets/css/fonts.css'
+    ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private flightsService: FlightsService, private registrationsService: RegistrationsService) {}
+
     public data: Array<IFlight> = [];
+    public registrations: string[] = [];
+
+    ngOnInit() {
+        this.getFlights();
+        this.getRegistrations();
+    }
+
+    private getFlights() {
+        this.flightsService.allFlights().subscribe(flights => {
+            this.data = flights;
+        });
+    }
+
+    private getRegistrations() {
+        this.registrationsService.allRegistrations().subscribe(registrations => this.registrations = registrations);
+    }
 }
