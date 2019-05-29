90f95e13e47368379a659e4b3e85c206ff828435
leoleonard <damian.juzyk@gmail.com>
2019-05-29 20:39:19 +0200

    add fix to header component

--- src/app/components/data-grid/header/header.component.ts
+++ src/app/components/data-grid/header/header.component.ts
@@ -1,12 +1,9 @@
-import { 
-    Component,
-    Input
-} from "@angular/core";
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: "grid-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.css"]
+  selector: 'grid-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class GridHeaderComponent {
     @Input() headers: string[];
--- src/app/components/data-grid/header/header.module.ts
+++ src/app/components/data-grid/header/header.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
-import { GridHeaderComponent } from "./header.component";
-import { CommonModule } from "@angular/common";
+import { GridHeaderComponent } from './header.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [GridHeaderComponent],
